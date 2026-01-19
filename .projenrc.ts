4c7b6b6f2dfb3038e3551bbdbcef61ac1749a980
Marnix Dessing <m.dessing@nijmegen.nl>
2026-01-19 14:43:31 +0100

    chore: setup .npmrc

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,5 +9,8 @@ const project = new GemeenteNijmegenTsPackage({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
+  gitignore: [
+    '.npmrc'
+  ]
 });
 project.synth();
